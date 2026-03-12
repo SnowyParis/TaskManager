c0010f9667b80703dde6ba29d1afd5e741514704
SnowyParis <146828233+SnowyParis@users.noreply.github.com>
2026-03-12 12:23:03 +0200

    uploaded controllers and views

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models.ViewModels;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
