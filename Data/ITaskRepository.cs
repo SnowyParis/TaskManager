558ef93e87362f13f1626cdbf8a1f18be09c596e
SnowyParis <146828233+SnowyParis@users.noreply.github.com>
2026-03-13 11:17:25 +0200

    Added repository files

---
+++ Data/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskManager.Models;
+
+namespace TaskManager.Data
+{
+    public interface ITaskRepository
+    {
+        IQueryable<TaskModel> GetAllTasks();
+        TaskModel GetTaskById(int taskId);
+        void AddTask(TaskModel task);
+        void UpdateTask(TaskModel task);
+        void DeleteTask(TaskModel task);
+        void SaveChanges();
+    }
+}
---
+++ Data/TaskRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO.Pipelines;
+using TaskManager.Models;
+
+namespace TaskManager.Data
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly TaskContext _taskContext;
+
+        public TaskRepository(TaskContext taskContext)
+        {
+            _taskContext = taskContext;
+        }
+
+        public IQueryable<TaskModel> GetAllTasks()
+        {
+            return _taskContext.Tasks;
+        }
+
+        public void AddTask(TaskModel task)
+        {
+            _taskContext.Tasks.Add(task);
+        }
+
+        public void DeleteTask(TaskModel task)
+        {
+            _taskContext.Tasks.Remove(task);
+        }
+
+        public TaskModel GetTaskById(int taskId)
+        {
+            return _taskContext.Tasks.FirstOrDefault(t => t.TaskID == taskId);
+        }
+
+        public void SaveChanges()
+        {
+            _taskContext.SaveChanges();
+        }
+
+        public void UpdateTask(TaskModel task)
+        {
+            _taskContext.Tasks.Update(task);
+        }
+    }
+}
